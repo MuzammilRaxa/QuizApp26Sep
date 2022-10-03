cac45ca65b2912d04ef2ed3450b0b713df12cb41
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-03 14:03:03 +0500

    finally completed without Firebase

--- quiz-app-final/src/Components/EndPage.js
+++ quiz-app-final/src/Components/EndPage.js
@@ -1,31 +1,66 @@
-import React from 'react'
-import { useContext } from 'react';
-import { QuizStateContext } from '../Helpers/Context'
-import { allQuestion } from '../Helpers/Questions';
+import React from "react";
+import { useContext } from "react";
+import { QuizStateContext } from "../Helpers/Context";
+import { allQuestion } from "../Helpers/Questions";
 
 function EndPage() {
-  const { correct, setCorrect, incorrect } = useContext(QuizStateContext);
+  const { correct, setCorrect, setScore, incorrect, setIncorrect, setQuizState, setQuestionIndex, } = useContext(QuizStateContext);
+
+
+  const playAgain = () => {
+    setScore(0)
+    setCorrect(0)
+    setQuestionIndex(0)
+    setIncorrect(0)
+    setQuizState('menu')
+  }
 
   return (
     <div className="result">
-      <h1 className='hResult'>Congratulation you have complete!! &#9996;</h1>
+      <h1 className="hResult">Congratulation you have complete!! &#9996;</h1>
 
       <div className="resultBox">
-        <h2> Success rate is {correct} into {allQuestion.length}</h2>
-        <h2 className='endResult' style={{ backgroundColor: '#b1ccff' }}>Score is {((100 / allQuestion.length) * correct)}% into 100% </h2>
 
-        {
-          correct > 0 ? (<div className='emojiSpace'><h2 className='endResult' style={{ backgroundColor: 'aquamarine' }} >Your Correct answer is {correct} &#128076;</h2> <span style={{ fontSize: '50px' }}> &#128150;</span></div>) :
-            (<h2 className='endResult' style={{ backgroundColor: 'pink' }} >Sad!! Correct answer is {correct} &#128148;</h2>)
-        }
-        {
-          incorrect > 0 ? (<div className='emojiSpace'><h2 className='endResult' style={{ backgroundColor: 'red' }} >Your incorrect is {incorrect} &#128148;</h2 > <span style={{ fontSize: '50px' }}>&#128533;</span></div>) :
-            (<h2 className='endResult' style={{ backgroundColor: 'pink' }} >Congratulation!! Your incorrect is {incorrect} &#128077;</h2 >)
-        }
+        <h2>
+          Success rate is {correct} into {allQuestion.length}
+        </h2>
 
+        <h2 className="endResult" style={{ backgroundColor: "#b1ccff", border: '3px solid rgb(68 87 96)' }}>
+          Score is {(100 / allQuestion.length) * correct}% into 100%{" "}
+        </h2>
+
+        {correct > 0 ? (
+          <div id="correctColor" className="emojiSpace">
+            <h2 className="endResult" style={{ backgroundColor: "aquamarine" }}>
+              Your Correct answer is {correct} &#128076;
+            </h2>
+            <span style={{ fontSize: "50px" }}> &#128150;</span>
+          </div>
+        ) : (
+          <div id="noCorrectColor" >
+            <h2 className="endResult" style={{ backgroundColor: "pink" }}>
+              Sad!! Correct answer is {correct} &#128148;
+            </h2>
+          </div>
+        )}
+        {incorrect > 0 ? (
+          <div id="incorrect" className="emojiSpace">
+            <h2 className="endResult" style={{ backgroundColor: "red" }}>
+              Your incorrect is {incorrect} &#128148;
+            </h2>
+            <span style={{ fontSize: "50px" }}>&#128533;</span>
+          </div>
+        ) : (
+          <div id="noIncorrect">
+            <h2 className="endResult" style={{ backgroundColor: "aquamarine" }}>
+              Congratulation!! Your incorrect is {incorrect} &#128077;
+            </h2>
+          </div>
+        )}
       </div>
-    </div >
-  )
+      <button id="btn" onClick={playAgain}>Play Again</button>
+    </div>
+  );
 }
 
-export default EndPage
+export default EndPage;
--- quiz-app-final/src/Components/StartPage.js
+++ quiz-app-final/src/Components/StartPage.js
@@ -1,23 +1,110 @@
-import '../App.css'
-import { useContext } from 'react';
-import { QuizStateContext } from '../Helpers/Context'
+import "../App.css";
+import { useContext, useState, useEffect } from "react";
+import { QuizStateContext } from "../Helpers/Context";
+
+// import {
+//     createUserWithEmailAndPassword,
+//     onAuthStateChanged,
+//     signOut,
+// } from "firebase/auth";
+// import { auth } from "../firebaseConfig";
+
+// firebase ===========>>>>>>>>>>>>>>
+
+// const [registerEmail, setRegisterEmail] = useState("");
+// const [registerPassword, setRegisterPassword] = useState("");
+// const [loginEmail, setLoginEmail] = useState("");
+// const [loginPassword, setLoginPassword] = useState("");
+
+// const [user, setUser] = useState({});
+
+// useEffect(
+//     onAuthStateChanged(auth, (currentUser) => {
+//         setUser(currentUser);
+//     }, [])
+// )
+
+// const register = async () => {
+//     try {
+//         const user = await createUserWithEmailAndPassword(
+//             auth,
+//             registerEmail,
+//             registerPassword
+//         );
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// };
+
+// console.log('user', user)
+
+// const login = async () => { };
+
+// const logout = async () => {
+//     await signOut(auth);
+// };
 
 function StartPage() {
     const { quizState, setQuizState } = useContext(QuizStateContext);
 
-
-    // var sectionStyle = {
-    //   width: "100%",
-    //   height: "400px",
-    //   backgroundImage: "url(" + { Background } + ")"
-    // };
-
     return (
-        <div className='startPage' >
+        <div className="startPage">
             <h1> Welcom to Page For Quiz, you can start. </h1>
-            <button id='btn' onClick={() => setQuizState('startQuiz')}> Start now </button>
+            <button id="btn" onClick={() => setQuizState("startQuiz")}>
+                {" "}
+                Start now{" "}
+            </button>
+
+            {/* <div className="formBox">
+                <div className="emailForm">
+                    <h3 className="formText">Email</h3>
+                    <input
+                        className="inputField"
+                        placeholder="Email..."
+                        onChange={(event) => {
+                            setRegisterEmail(event.target.value);
+                        }} />
+
+                    <input
+                        className="inputField"
+                        placeholder="Password..."
+                        onChange={(event) => {
+                            setRegisterPassword(event.target.value);
+                        }} />
+
+                    <button className="btn" onClick={register}>
+                        Creat User
+                    </button>
+                </div>
+                <div className="emailForm">
+                    <h3 className="formText">Login</h3>
+                    <input
+                        className="inputField"
+                        placeholder="Login..."
+                        onChange={(event) => {
+                            setLoginEmail(event.target.value);
+                        }} />
+
+                    <input
+                        className="inputField"
+                        placeholder="Password..."
+                        onChange={(event) => {
+                            setLoginPassword(event.target.value);
+                        }} />
+
+                    <button className="btn">Login</button>
+                </div>
+                <div className="emailForm">
+                    <h4 className="formText">User Logged In</h4>
+                    <h4>{auth.currentUser.email}</h4>
+                    <button className="btn" onClick={logout}>
+                        Sign Out
+                    </button>
+                </div>
+            </div> */}
+
         </div>
-    )
+    );
 }
 
-export default StartPage
+export default StartPage;
